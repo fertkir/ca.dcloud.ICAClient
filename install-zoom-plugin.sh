91f345b1f3900b424db015973b7fe7f770e73037
Kirill Fertikov <kirill.fertikov@gmail.com>
2023-07-03 22:33:31 +0500

    zoom plugin

--- install-zoom-plugin.sh
+++ install-zoom-plugin.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-wget -O /tmp/zoomplugin.deb https://zoom.us/download/vdi/5.14.0.23370/zoomcitrixplugin-ubuntu_5.14.0.deb
+mkdir -p /tmp/zoomplugin
+wget -O /tmp/zoomplugin/zoomplugin.deb https://zoom.us/download/vdi/5.14.0.23370/zoomcitrixplugin-ubuntu_5.14.0.deb
+
+cd /tmp/zoomplugin/
+ar -x zoomplugin.deb
+tar -xf data.tar.xz
+tar -xf control.tar.xz
+
+mkdir -p /app/etc/
+mv etc/* /app/etc/
+
+mv opt/Citrix/ICAClient/* /app/ICAClient/linuxx64/
+
+mkdir -p /app/usr/lib/
+mv usr/lib/* /app/usr/lib/
+
+mkdir -p /app/usr/share/doc/
+mv usr/share/doc/* /app/usr/share/doc/
+
+sed -i 's/CONFIG_LINK_FILE=.*$/CONFIG_LINK_FILE=\/app\/ICAClient\/linuxx64\/config\/module.ini/' postinst
+. postinst
