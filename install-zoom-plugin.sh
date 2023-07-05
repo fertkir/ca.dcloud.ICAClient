ae31ddcc60a78cef16896400ec305909cda253de
Kirill Fertikov <kirill.fertikov@gmail.com>
2023-07-06 00:32:31 +0500

    final attempt

--- install-zoom-plugin.sh
+++ install-zoom-plugin.sh
@@ -8,16 +8,16 @@ ar -x zoomplugin.deb
 tar -xf data.tar.xz
 tar -xf control.tar.xz
 
-mkdir -p /app/etc/
-mv etc/* /app/etc/
+#mkdir -p /app/etc/
+mv etc/zoomvdi/ZoomMedia.ini /app/ICAClient/linuxx64/config/
+sed -i 's/^PATH=.*$/PATH=\/app\/usr\/lib\/zoomcitrixplugin\//' /app/ICAClient/linuxx64/config/ZoomMedia.ini
+sed -i 's/^LD_LIBRARY_PATH=.*$/LD_LIBRARY_PATH=\/app\/usr\/lib\/zoomcitrixplugin\/Qt\/lib\//' /app/ICAClient/linuxx64/config/ZoomMedia.ini
+sed -i 's/^OS_DISTRO=.*$/OS_DISTRO=centos/' /app/ICAClient/linuxx64/config/ZoomMedia.ini
 
 mv opt/Citrix/ICAClient/* /app/ICAClient/linuxx64/
 
 mkdir -p /app/usr/lib/
 mv usr/lib/* /app/usr/lib/
-
-mkdir -p /app/usr/share/doc/
-mv usr/share/doc/* /app/usr/share/doc/
 
 sed -i 's/CONFIG_LINK_FILE=.*$/CONFIG_LINK_FILE=\/app\/ICAClient\/linuxx64\/config\/module.ini/' postinst
 . postinst
